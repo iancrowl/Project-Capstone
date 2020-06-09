9da58f2b08b5d9d319abcfb99258ec044af8b8b8
Ian Crowl <icrowl@relias.com>
2020-06-09 08:43:57 -0400

    modified docker image name

--- scripts/run-kubernetes.sh
+++ scripts/run-kubernetes.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath="sabbir33/capstone-app"
+dockerpath="icrowl/project-capstone-app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run capstone-app --image=$dockerpath --port=80
+kubectl run project-capstone-app --image=$dockerpath --port=80
 
 
 # Step 3:
@@ -18,7 +18,7 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment capstone-app --type=LoadBalancer --port=80
+kubectl expose deployment project-capstone-app --type=LoadBalancer --port=80
 
 # Open the service 
-# minikube service capstone-app
+# minikube service project-capstone-app
