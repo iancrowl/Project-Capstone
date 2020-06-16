de2f29efb7bd47c2a75b7969bf66dad9ac959605
Ian Crowl <icrowl@relias.com>
2020-06-15 20:53:21 -0400

    uncommented out last line

--- scripts/create-ekscluster.sh
+++ scripts/create-ekscluster.sh
@@ -3,4 +3,4 @@ aws cloudformation create-stack \
 --template-body file:///home/ubuntu/Project-Capstone/CloudFormation/eks-cluster.yml \
 --parameters file:///home/ubuntu/Project-Capstone/CloudFormation/eks-cluster-params.json \
 --region us-east-2 \
-#--capabilities CAPABILITY_NAMED_IAM
+--capabilities CAPABILITY_NAMED_IAM
