76964ed40aa5148a910b29ecaa59c918efee5a4a
Ian Crowl <icrowl@relias.com>
2020-06-15 20:44:41 -0400

    updated path in create-network

--- scripts/create-network.sh
+++ scripts/create-network.sh
@@ -1,6 +1,6 @@
 aws cloudformation create-stack \
 --stack-name project-capstone-network \
---template-body file://home/ubuntu/Project-Capstone/CloudFormation/network.yml \
---parameters file://home/ubuntu/Project-Capstone/CloudFormation/network-params.yml \
+--template-body file:///home/ubuntu/Project-Capstone/CloudFormation/network.yml \
+--parameters file:///home/ubuntu/Project-Capstone/CloudFormation/network-params.yml \
 --region us-east-2 \
 #--capabilities CAPABILITY_NAMED_IAM
