8bb11d255a502f34ef28d39e84e8e2067be09f6b
Ian Crowl <icrowl@relias.com>
2020-06-16 08:38:33 -0400

    uncommented out last line

--- scripts/create-network.sh
+++ scripts/create-network.sh
@@ -3,4 +3,4 @@ aws cloudformation create-stack \
 --template-body file:///home/ubuntu/Project-Capstone/CloudFormation/network.yml \
 --parameters file:///home/ubuntu/Project-Capstone/CloudFormation/network-params.json \
 --region us-east-2 \
-#--capabilities CAPABILITY_NAMED_IAM
+--capabilities CAPABILITY_NAMED_IAM
