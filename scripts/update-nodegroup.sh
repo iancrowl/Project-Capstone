b1322e904785fb5da7f89896b34af8fc9279f559
Ian Crowl <icrowl@relias.com>
2020-06-15 21:32:27 -0400

    added update-nodegroup.sh

---
+++ scripts/update-nodegroup.sh
@@ -0,0 +1,6 @@
+aws cloudformation update-stack \
+--stack-name project-capstone-nodegroup \
+--template-body file:///home/ubuntu/Project-Capstone/CloudFormation/eks-nodegroup.yml \
+--parameters file:///home/ubuntu/Project-Capstone/CloudFormation/eks-nodegroup-params.json \
+--region us-east-2 \
+--capabilities CAPABILITY_NAMED_IAM
