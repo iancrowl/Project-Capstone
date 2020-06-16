9fcb26608f6956c50129da4a0efd8dc35bd2222b
Ian Crowl <icrowl@relias.com>
2020-06-15 21:16:28 -0400

    updated template-body file name

--- scripts/create-nodegroup.sh
+++ scripts/create-nodegroup.sh
@@ -1,6 +1,6 @@
 aws cloudformation create-stack \
 --stack-name project-capstone-nodegroup \
---template-body file:///home/ubuntu/Project-Capstone/CloudFormation/eks-nodegroup.sh \
+--template-body file:///home/ubuntu/Project-Capstone/CloudFormation/eks-nodegroup.yml \
 --parameters file:///home/ubuntu/Project-Capstone/CloudFormation/eks-nodegroup-params.json \
 --region us-east-2 \
 --capabilities CAPABILITY_NAMED_IAM
